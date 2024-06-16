35d1504dc5c3a736b815d6bc7fdad0a598ca29e1
aryanPatel <aryanpatel1248@gmail.com>
2024-06-16 11:17:55 +0530

    first commit of the day work on responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,24 +6,25 @@ import { skills } from '../data/skills';
 const About = () => {
   return (
     <section className="about" id="about">
-      <div className="about-moldure">
-        <div className="about-content">
+        <div className="about-moldure">
+        <div className="img-about">
+          <img src={me} alt="About"  />
+        </div>
+          <div className="about-content">
           <h1>About Me</h1>
           <h2>An ambitious Front-End developer, who aspires to success 🎓</h2>
           <p>
             I am a skilled, creative, and analytical programmer with a nimble mind to solve problems and
             translate ideas into functional code. My collaboration and passion for programming, coupled with
             my ability to learn quickly, make me a valuable asset to any development team.
-          </p>
+          </p><div className='skillContainer'>
           <h3 id='skillHead'>Skills:</h3>
           <div className="skills">
             {skills.map(skill => (
               <img  className="skillImg"key={skill.name} src={skill.icon} alt={skill.name} />
             ))}
           </div>
-        </div>
-        <div className="img-about">
-          <img src={me} alt="About"  />
+          </div>
         </div>
       </div>
     </section>
