4818bde8e0e74c934be2cad39ebbe0100c3ffc3b
aryanPatel <aryanpatel1248@gmail.com>
2024-06-17 11:14:15 +0530

    small j path problem resolve

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,7 +2,7 @@ import htmlIcon from "../assets/imgs/html.png"
 import cssIcon from "../assets/imgs/css.png"
 import javascriptIcon from "../assets/imgs/js.jpg"
 import reactJs from "../assets/imgs/reactJs.png"
- import nodeJs from "../assets/imgs/nodeJs2.png"
+ import nodeJs from "../assets/imgs/nodejs2.png"
 export const skills = [
     {
       name: 'HTML',
